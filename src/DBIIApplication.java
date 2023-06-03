707c8cc38f36f4967656629c3ecdbd03208ecdd2
mateusVedoy <mateusvedoy@outlook.com>
2023-06-03 17:56:52 -0300

    main class

---
+++ src/DBIIApplication.java
@@ -0,0 +1,12 @@
+import application.WindowDialog.InteractiveMenu;
+
+/*
+* Aluno: Mateus Vedoy e Rodigo Presa
+* Turma: SSI noturno
+* Disciplina: DB II
+* */
+public class DBIIApplication {
+    public static void main(String[] args) {
+        InteractiveMenu.execute();
+    }
+}
