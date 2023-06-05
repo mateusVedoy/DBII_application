0a750bca0498ade7b20ead53b75abf59c80cc8f8
mateusVedoy <mateusvedoy@outlook.com>
2023-06-05 15:32:24 -0300

    atualiza bind de procedures

--- src/infraestructure/database/SQLs.java
+++ src/infraestructure/database/SQLs.java
@@ -1,21 +1,28 @@
 package infraestructure.database;
 
 public enum SQLs {
-    INTERACTION_AD_BY_USER("select * from table(interacoes_usuario_anuncios(?);"),
-    COUNT_AVG_PROPOSES_BY_PROPERTY("select * from table(propostas_anuncio_imovel(?));"),
+    INTERACTION_AD_BY_USER("select * from table(interacoes_usuario_anuncios(?))"),
+    COUNT_AVG_PROPOSES_BY_PROPERTY("select * from table(propostas_anuncio_imovel(?))"),
     INSERT_USER("DECLARE\n" +
             "  v_codigo INTEGER := seq_usuario.nextval;\n" +
             "  v_ativo VARCHAR2(10) := '1';\n" +
-            "  v_nomecompleto VARCHAR2(50) := 'João';\n" +
-            "  v_cpf VARCHAR2(20) := '323.347.086-24';\n" +
-            "  v_renda NUMBER := 1200;\n" +
-            "  v_tipoperfil INTEGER := 1;\n" +
+            "  v_nomecompleto VARCHAR2(50) := ?;\n" +
+            "  v_cpf VARCHAR2(20) := ?;\n" +
+            "  v_renda NUMBER := ?;\n" +
+            "  v_tipoperfil INTEGER := ?;\n" +
             "  v_msg VARCHAR2(200);\n" +
             "BEGIN\n" +
             "  insere_usuario(v_codigo, v_ativo, v_nomecompleto, v_cpf, v_renda, v_tipoperfil, v_msg);\n" +
             "  DBMS_OUTPUT.PUT_LINE(v_msg);\n" +
             "END;"),
-    LIST_ALL_PROPERTIES_WITH_ACTIVE_AD("EXEC listar_imoveis_com_anuncio_ativo");
+    UPDATE_USER_PROFILE_TYPE("DECLARE\n" +
+            "  v_codigo INT := ?;\n" +
+            "  v_tipoperfil INT := ?;\n" +
+            "BEGIN\n" +
+            "  atualiza_tipoperfil(v_codigo, v_tipoperfil);\n" +
+            "END;"),
+
+    SELECT_ALL_USERS("select * from usuario");
 
     private final String sql;
 
