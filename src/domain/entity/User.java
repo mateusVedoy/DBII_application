eb06c0c13e51581e2701be1ea9577a70c99c4554
mateusVedoy <mateusvedoy@outlook.com>
2023-06-05 15:33:30 -0300

    gera entidade Usuario

---
+++ src/domain/entity/User.java
@@ -0,0 +1,63 @@
+package domain.entity;
+
+public class User {
+    private int id;
+    private String active;
+    private String fullName;
+    private String cpf;
+    private double income;
+    private int profileType;
+
+    public User(int id, String active, String fullName, String cpf, double income, int profileType) {
+        this.id = id;
+        this.active = active;
+        this.fullName = fullName;
+        this.cpf = cpf;
+        this.income = income;
+        this.profileType = profileType;
+    }
+
+    public User(String active, String fullName, String cpf, double income, int profileType) {
+        this.active = active;
+        this.fullName = fullName;
+        this.cpf = cpf;
+        this.income = income;
+        this.profileType = profileType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getActive() {
+        return active;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public double getIncome() {
+        return income;
+    }
+
+    public int getProfileType() {
+        return profileType;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", active='" + active + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", income=" + income +
+                ", profileType=" + profileType +
+                '}';
+    }
+}
