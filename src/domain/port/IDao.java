b3213520bf722315801c57477fa1d1b61427bd22
mateusVedoy <mateusvedoy@outlook.com>
2023-06-05 15:33:06 -0300

    atualiza assinaturas de métodos na port DAO

--- src/domain/port/IDao.java
+++ src/domain/port/IDao.java
@@ -1,6 +1,7 @@
 package domain.port;
 
 import domain.entity.ProposesAndAvgAmountByProperty;
+import domain.entity.User;
 import domain.entity.UserInteractionsWithAd;
 
 import java.util.List;
@@ -9,4 +10,7 @@ public interface IDao {
 
     List<UserInteractionsWithAd> getUserInteractionsWithAdByUserId(int userId);
     ProposesAndAvgAmountByProperty getProposesAndAvgAmountByPropertyId(int propertyId);
+    boolean insertUser(User user);
+    boolean updateProfileTypeFromUser(User user);
+    List<User> GetAllUsers();
 }
